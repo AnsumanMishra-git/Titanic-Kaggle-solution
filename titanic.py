bc103fcab3f1b7e59d8e5f92aead865c2accdb80
Ansuman Mishra <56594734+AnsumanMishra-git@users.noreply.github.com>
2021-03-30 02:23:51 +0530

    feature engineering done

--- titanic.py
+++ titanic.py
@@ -21,7 +21,7 @@ td = pd.concat([trd, tsd], ignore_index=True, sort  = False)
 
 trd.isnull().sum()
 tsd.isnull().sum()
-
+td.isnull().sum()
 
 #drop ticket no. it is useless
 td=td.drop(['Ticket'],axis=1)
@@ -34,16 +34,6 @@ sns.countplot('Pclass',data=trd,hue='Survived') #survivality of 3rd class is ver
 
 sns.countplot('Sex',data=trd,hue='Survived') #survivality of male is very less
 
-#max age was 80
-bins=[0,10,20,30,40,50,60,70,80]
-group=['0-10','10-20','20-30','30-40','40-50','50-60','60-70','70-80']
-td['age_bin']=pd.cut(td['Age'],bins,labels=group)
-age_bin=pd.crosstab(td['age_bin'],td['Survived'])
-age_bin.div(age_bin.sum(1).astype(float),axis=0).plot(kind='bar',stacked=True)
-plt.xlabel('Age Group')
-plt.ylabel('Percent')
-
-sns.countplot(x="age_bin", hue="Survived", data=td) #survivality of age 20-30 is very less , compared to others
 
 #max SibSp=8,min=0
 bins=[0,1,2,3,4,5,6,7,8]
@@ -66,7 +56,7 @@ sns.countplot('Fare_bin',data=td,hue='Survived') #abv_avg and high fare prices g
 
 
 sns.countplot('Embarked',data=td,hue='Survived')
-# %OF survivality in C(57)>Q(33)=S(33) approx
+# % of survivality in C(57)>Q(33)=S(33) approx
 
 #Feature Engineering
 
@@ -76,15 +66,78 @@ td['Title'] = td['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
 td['Title'].value_counts()
 
 
+title_map = {"Mr": 0, "Miss": 1, "Mrs": 2, 
+                 "Master": 3, "Dr": 4, "Rev": 4, "Col": 4, "Major": 4, "Mlle": 4,"Countess": 4,
+                 "Ms": 4, "Lady": 4, "Jonkheer": 4, "Don": 4, "Dona" : 4, "Mme": 4,"Capt": 4,"Sir": 4 }
+
+td['Title'] = td['Title'].map(title_map)
+
+sns.countplot('Title',data=td,hue='Survived') #survivality order Mrs>Miss>Master>Mr
+
+# now we do not require the name col , so we can drop it.
+td=td.drop(['Name'],axis=1)
+
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+cols=['Sex',['alone']]
+for col in cols:
+    td[col]=le.fit_transform(td[col])
+
+#filling the age 
+# We use the median age for each title (Mr, Mrs, Miss,Master, Others)
+td["Age"].fillna(td.groupby("Title")["Age"].transform("median"), inplace=True)
+td.isnull().sum()
+
+#max age was 80
+bins=[0,10,20,30,40,50,60,70,80]
+group=['0-10','10-20','20-30','30-40','40-50','50-60','60-70','70-80']
+td['age_bin']=pd.cut(td['Age'],bins,labels=group)
+age_bin=pd.crosstab(td['age_bin'],td['Survived'])
+age_bin.div(age_bin.sum(1).astype(float),axis=0).plot(kind='bar',stacked=True)
 
 
+sns.countplot(x="age_bin", hue="Survived", data=td) #survivality of age 20-30 is very less , compared to others
 
+#Embarked
+Class1=td[td['Pclass']==1]['Embarked'].value_counts()
+Class2=td[td['Pclass']==2]['Embarked'].value_counts()
+Class3=td[td['Pclass']==3]['Embarked'].value_counts()
+df=pd.DataFrame([Class1,Class2,Class3])
+df.index=['1stClass','2ndClass','3rdClass']
+df.plot(kind='bar', stacked=True)
+#conclusion- in all 3 classes we have more that 50% as S Embarked
 
+#filling Embarked
+td['Embarked']=td['Embarked'].fillna('S')
 
+#Encoding embarked
+td['Embarked']=le.fit_transform(td['Embarked'])
 
+# fill missing Fare with median fare for each Pclass
+td["Fare"].fillna(td.groupby("Pclass")["Fare"].transform("median"), inplace=True)
 
+#dropping cabin as it has a lot of missing values
+td=td.drop(['Cabin','SibSp_bin','Parch_bin','Fare_bin'],axis=1)
+td=td.drop(['age_bin'],axis=1)
+td=td.drop(['Parch','SibSp','family'],axis=1)
 
+#Making an Age Class 
 
+td['Age_bin'] = pd.cut(td['Age'], 5)
+td[['Age_bin', 'Survived']].groupby(['Age_bin'], as_index=False).mean().sort_values(by='Age_bin', ascending=True)
+td.loc[ td['Age'] <= 16, 'Age'] = 0
+td.loc[(td['Age'] > 16) & (td['Age'] <= 32), 'Age'] = 1
+td.loc[(td['Age'] > 32) & (td['Age'] <= 48), 'Age'] = 2
+td.loc[(td['Age'] > 48) & (td['Age'] <= 64), 'Age'] = 3
+td.loc[ td['Age'] > 64, 'Age']=4
 
+#Making an Fare Class
+td['Fare_bin'] = pd.qcut(td['Fare'], 4)
+td[['Fare_bin', 'Survived']].groupby(['Fare_bin'], as_index=False).mean().sort_values(by='Fare_bin', ascending=True)
 
+td.loc[ td['Fare'] <= 7.91, 'Fare'] = 0
+td.loc[(td['Fare'] > 7.91) & (td['Fare'] <= 14.454), 'Fare'] = 1
+td.loc[(td['Fare'] > 14.454) & (td['Fare'] <= 31), 'Fare']   = 2
+td.loc[ td['Fare'] > 31, 'Fare'] = 3
 
+td=td.drop(['Fare_bin','Age_bin'],axis=1)
