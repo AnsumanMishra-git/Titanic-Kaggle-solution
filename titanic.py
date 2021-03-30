260493a1762a2950a9747367418960effdac439b
Ansuman Mishra <56594734+AnsumanMishra-git@users.noreply.github.com>
2021-03-30 12:55:42 +0530

    Modelling done
    trained my model on logistic regression , SVM , Decision Tree and Random Forest . Got the result as 77%

--- titanic.py
+++ titanic.py
@@ -51,6 +51,8 @@ td['family']=td['Parch']+td['SibSp']
 td['alone']=td['family']==0 
 #travelling alone had high chance of survivality
 
+td=td.drop(['Parch','SibSp','family'],axis=1)
+
 td['Fare_bin'] = pd.cut(td['Fare'], bins=[0,7.90,14.45,31.28,120],labels=['Low','Mid','abv_avg','High'])
 sns.countplot('Fare_bin',data=td,hue='Survived') #abv_avg and high fare prices gave more chances of surivavilty
 
@@ -117,9 +119,7 @@ td['Embarked']=le.fit_transform(td['Embarked'])
 td["Fare"].fillna(td.groupby("Pclass")["Fare"].transform("median"), inplace=True)
 
 #dropping cabin as it has a lot of missing values
-td=td.drop(['Cabin','SibSp_bin','Parch_bin','Fare_bin'],axis=1)
-td=td.drop(['age_bin'],axis=1)
-td=td.drop(['Parch','SibSp','family'],axis=1)
+td=td.drop(['Cabin','SibSp_bin','Parch_bin','Fare_bin','age_bin'],axis=1)
 
 #Making an Age Class 
 
@@ -131,13 +131,146 @@ td.loc[(td['Age'] > 32) & (td['Age'] <= 48), 'Age'] = 2
 td.loc[(td['Age'] > 48) & (td['Age'] <= 64), 'Age'] = 3
 td.loc[ td['Age'] > 64, 'Age']=4
 
-#Making an Fare Class
-td['Fare_bin'] = pd.qcut(td['Fare'], 4)
+#Making a Fare Class
+td['Fare_bin'] = pd.qcut(td['Fare'], 5)
 td[['Fare_bin', 'Survived']].groupby(['Fare_bin'], as_index=False).mean().sort_values(by='Fare_bin', ascending=True)
 
-td.loc[ td['Fare'] <= 7.91, 'Fare'] = 0
-td.loc[(td['Fare'] > 7.91) & (td['Fare'] <= 14.454), 'Fare'] = 1
-td.loc[(td['Fare'] > 14.454) & (td['Fare'] <= 31), 'Fare']   = 2
-td.loc[ td['Fare'] > 31, 'Fare'] = 3
+td.loc[ td['Fare'] <= 7.854, 'Fare'] = 0
+td.loc[(td['Fare'] > 7.854) & (td['Fare'] <= 10.5), 'Fare'] = 1
+td.loc[(td['Fare'] >10.5) & (td['Fare'] <=  21.558), 'Fare']   = 2
+td.loc[(td['Fare'] > 21.558) & (td['Fare'] <=  41.579), 'Fare']   = 3
+td.loc[ td['Fare'] > 41.579, 'Fare'] = 4
 
 td=td.drop(['Fare_bin','Age_bin'],axis=1)
+td.isnull().sum()
+
+#drop passengerId
+td=td.drop(['PassengerId'],axis=1)
+#splitting x_train and x_test
+x_test = td[td.Survived.isnull()]
+x_test = x_test.drop(['Survived'], axis = 1)
+
+x_train=td.dropna()
+y_train = td['Survived'].dropna()
+x_train= x_train.drop(['Survived'], axis = 1)
+
+#splitting x_train into training and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test =train_test_split(x_train,y_train,test_size=0.25 , random_state=42) 
+
+#Modelling
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+x_train.isnull().sum()
+y_train.isnull().sum()
+x_test.isnull().sum()
+
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, Y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(Y_test, y_pred)
+print(cm)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = Y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+# from logistic regression i got 79.80%
+
+
+
+# Training the SVM model on the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear',random_state = 0)
+classifier.fit(X_train, Y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(Y_test, y_pred)
+print(cm)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = Y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+# from SVM i got 78.75%
+
+
+# Training the Decision Tree model on the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion='entropy',random_state = 0)
+classifier.fit(X_train, Y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(Y_test, y_pred)
+print(cm)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = Y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+# from Decision Tree i got 77.99%%
+
+
+
+# Training the Random Forest model on the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=10,criterion='entropy',random_state = 0)
+classifier.fit(X_train, Y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(Y_test, y_pred)
+print(cm)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = Y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+# from Random Forest i got 80.55% WINNER
+
+
+#Predicting the actual test set
+y_pred = classifier.predict(x_test)
+
+#creating submission file
+og_test_set=pd.read_csv("test.csv")
+sol=pd.read_csv("submit.csv")
+sol["Survived"]=y_pred
+sol["PassengerId"]=og_test_set["PassengerId"]
+
+#converting to csv file
+pd.DataFrame(sol,columns=["PassengerId","Survived"]).to_csv("final_answer.csv")
+
+# Result - 77.03%
+
+
+
+
+
+
